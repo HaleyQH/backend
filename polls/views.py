9478c590e50c2099e4d3c075eede15f468398aa3
Haley <haleyqh@gmail.com>
2021-01-13 18:21:29 +0800

    2021.1.13

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from bson.objectid import ObjectId
 from rest_framework.decorators import action
 # Create your views here.
+from 检索组pip包.源代码.search_utils import get_suggestion, search
 from .models import UserModel
 from .serializers import UserSerializer
 from rest_framework.response import Response
@@ -18,31 +19,24 @@ class UserViewSet(ModelViewSet):
 
     @action(detail=False, methods=['get'])
     def query(self, request):
-        theme = self.request.query_params.get('theme', None)
+        theme = self.request.query_params.getlist('theme[]', None)
+        theme=','.join(theme)
         query = self.request.query_params.get('query', None)
-        data = [{'title': '基于深度学习的视频字幕技术研究',
-                 'author': '刘千会',
-                 'video': 'E:\\video.mp4',
-                 }, {
-                    'title': '基于深度学习的视频字幕技术研究',
-                    'author': '刘千会',
-                    'video': 'E:\\video.webm',
-                }]
+        data = search(query, theme=theme)
         return Response(data)
 
-        # theme = self.request.query_params.get('theme', None)
-        # query = self.request.query_params.get('query', None)
-        # queryset = {
-        #     'author': lambda x: UserModel.objects.all().filter(author=x),
-        #     'title': lambda x: UserModel.objects.all().filter(title=x),
-        # }[theme](query)
-        #
-        #
-        # serializer = UserSerializer(queryset, many=True, context={'request': request})
-        # return Response(serializer.data)
+        # data = [{'title': '基于深度学习的视频字幕技术研究',
+        #          'author': '刘千会',
+        #          'video': 'E:\\video.mp4',
+        #          }, {
+        #             'title': '基于深度学习的视频字幕技术研究',
+        #             'author': '刘千会',
+        #             'video': 'E:\\video.webm',
+        #         }]
+        # return Response(data)
 
     @action(detail=False, methods=['get'])
-    def video(self, request):
-        video = self.request.query_params.get('video', None)
-        data = open(video, 'rb')
-        return StreamingHttpResponse(data, content_type='video/mp4')
+    def hint(self, request):
+        query = self.request.query_params.get('query', None)
+        data = get_suggestion(query)
+        return Response(data)
